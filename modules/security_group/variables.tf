f148255bb67a46fc23e744ad2b30d83923a0fd70
AbdelrahmanElshamy <abdelrahmanmedhat79@gmail.com>
2024-10-20 01:14:35 +0300

    Update variables.tf

--- modules/security_group/variables.tf
+++ modules/security_group/variables.tf
@@ -6,12 +6,27 @@ variable "vpc_id" {
   description = "The VPC ID where the security group will be created"
 }
 
+# ```modules/security_group/variables.tf
 variable "ingress_rules" {
   description = "A list of ingress rules"
   type = list(object({
     from_port   = number
     to_port     = number
     protocol    = string
-    cidr_blocks = list(string)
+    cidr_blocks = list(string) // This should contain valid CIDR blocks
   }))
+  default = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"] // Example CIDR block for HTTP
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["192.168.1.0/24"] // Example CIDR block for SSH
+    }
+  ]
 }
