1f213cf4d05234514167e32dadad0a3c833241ed
AbdelrahmanElshamy <abdelrahmanmedhat79@gmail.com>
2024-10-20 01:14:10 +0300

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -9,7 +9,10 @@ resource "aws_security_group" "this" {
       from_port   = ingress.value.from_port
       to_port     = ingress.value.to_port
       protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
+      cidr_blocks = [
+          "0.0.0.0/0", // Added valid CIDR block
+          "0.0.0.0/0"  // Added valid CIDR block
+      ]
     }
   }
 
