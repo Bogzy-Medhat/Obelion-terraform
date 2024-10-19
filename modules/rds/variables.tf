d5f2c805882881a19bf7981a91f53344bec10cdc
AbdelrahmanElshamy <abdelrahmanmedhat79@gmail.com>
2024-10-20 01:13:45 +0300

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,14 +1,17 @@
 variable "db_name" {
   description = "The name of the database"
+  default     = "mydb"
 }
 
 variable "db_username" {
   description = "The username for the database"
+  default     = "admin"
 }
 
 variable "db_password" {
   description = "The password for the database"
   sensitive   = true
+  default     = "password123"
 }
 
 variable "security_group_id" {
@@ -16,6 +19,7 @@ variable "security_group_id" {
 }
 
 variable "subnet_ids" {
-  description = "The subnet IDs for the RDS instance"
+  description = "List of subnet IDs for the RDS DB subnet group"
   type        = list(string)
+  default     = ["subnet-07c981f34209f3527", "subnet-039b8005827ad4dea"] # Replace with actual subnet IDs from different AZs
 }
