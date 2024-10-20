888985bdbc3ebe38a321817e03c04c6bacd9f529
AbdelrahmanElshamy <abdelrahmanmedhat79@gmail.com>
2024-10-20 18:03:50 +0300

    Update main.tf

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -15,6 +15,21 @@ resource "aws_instance" "backend" {
   tags = {
     Name = "backend-machine"
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y docker.io
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ${var.username} // Use the correct variable for the username
+              sudo apt install -y composer // Added Composer installation
+              curl -sS https://getcomposer.org/installer | php // Download Composer
+              sudo mv composer.phar /usr/local/bin/composer // Move Composer to bin
+              sudo add-apt-repository ppa:ondrej/php // Add PHP repository
+              sudo apt update // Update package list again
+              sudo apt install -y php8.2 php8.2-cli php8.2-fpm php8.2-mbstring php8.2-xml php8.2-curl php8.2-bcmath // Install PHP packages
+              EOF
 }
 
 output "public_ip" {
@@ -23,4 +38,9 @@ output "public_ip" {
 
 output "instance_id" {
   value = aws_instance.backend.id
+}
+
+variable "username" {
+  description = "The username to be added to the Docker group"
+  type        = string
 }
