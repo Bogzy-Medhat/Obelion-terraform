721a0b4e4df3fcecb2e54df443d5575718ba1fad
AbdelrahmanElshamy <abdelrahmanmedhat79@gmail.com>
2024-10-21 17:53:44 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.32.0/24", "10.0.33.0/24"] // Added default value
+}
+
 module "vpc" {
   source              = "./modules/vpc"
   vpc_cidr_block      = "10.0.0.0/16"
   vpc_name            = "my-vpc"
-  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnet_cidrs = var.public_subnet_cidrs
 }
 
 output "vpc_cidr_block" {
@@ -52,21 +63,6 @@ module "frontend_sg" {
     }
   ]
 }
-
-module "rds_sg" {
-  source = "./modules/security_group"
-  name   = "rds-sg"
-  vpc_id = module.vpc.vpc_id
-  ingress_rules = [
-    {
-      from_port   = 3306
-      to_port     = 3306
-      protocol    = "tcp"
-      cidr_blocks = module.vpc.public_subnet_ids
-    }
-  ]
-}
-
 module "backend" {
   source            = "./modules/backend"
   ami_id            = "ami-005fc0f236362e99f"
@@ -87,6 +83,20 @@ module "frontend" {
   username          = "ubuntu"
 }
 
+module "rds_sg" {
+  source = "./modules/security_group"
+  name   = "rds-sg"
+  vpc_id = module.vpc.vpc_id
+  ingress_rules = [
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"] # Use proper CIDR blocks, not subnet IDs
+    }
+  ]
+}
+
 module "rds" {
   source            = "./modules/rds"
   vpc_id            = module.vpc.vpc_id
@@ -95,7 +105,9 @@ module "rds" {
   db_password       = "password123"
   security_group_id = module.rds_sg.id
   subnet_ids        = module.vpc.public_subnet_ids
-  vpc_cidr          = module.vpc.vpc_cidr_block // Uncommented this line to provide the required argument
+  vpc_cidr          = module.vpc.vpc_cidr_block
+  public_subnet_cidrs = var.public_subnet_cidrs
+  vpc_cidr_block    = module.vpc.vpc_cidr_block
 }
 
 module "cloudwatch_backend" {
