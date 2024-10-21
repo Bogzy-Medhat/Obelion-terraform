ae49b69b230deb234a231411fac2432b47b428f4
AbdelrahmanElshamy <abdelrahmanmedhat79@gmail.com>
2024-10-21 17:53:26 +0300

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,6 +1,7 @@
 variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+  type        = string
+  default     = "10.0.0.0/16" // Set your desired default value
 }
 
 variable "vpc_name" {
